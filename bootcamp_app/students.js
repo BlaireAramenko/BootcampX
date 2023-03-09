466465244a7ffe43a447eca9f02010cd6afd4865
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-09 05:56:53 -0800

    add parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,6 +10,7 @@ const pool = new Pool({
 
 
 
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
@@ -22,3 +23,20 @@ LIMIT ${process.argv[3] || 5};
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     })
   }).catch(err => console.error('query error', err.stack));
+
+
+//parameterized query
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+pool.query(queryString, values);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+//database connection
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -21,3 +22,19 @@ ORDER BY teacher;
       console.log(`${row.cohort}: ${row.teacher}`);
     })
   });
+
+//parameterized query
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+pool.query(queryString, values);
